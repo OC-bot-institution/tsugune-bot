417a6fc2ed3b807a4ed211a77d0cdcd8838285ff
hamu132 <guba06120@gmail.com>
2026-08-09 11:53:08 +0900

    時間指定できるようにした

--- main_web.py
+++ main_web.py
@@ -166,23 +166,37 @@ async def on_message(message):
 
 @bot.tree.command(
     name="sleep",
-    description="30分寝ます"
+    description="しばらく反応しなくなります"
 )
-async def sleep(interaction: discord.Interaction):
-
+async def sleep(
+    interaction: discord.Interaction,
+    minutes: int
+):
     global bot_status
+
+    if bot_status=="sleep":
+        await interaction.response.send_message("ぐーすか")
+        return
 
     bot_status = "sleep"
 
     await interaction.response.send_message(
-        "30分だけ寝ますおやすみ！！！！！"
+        f"{minutes}分だけ寝ます。おやすみ！！！！！"
     )
 
-    await asyncio.sleep(1800)
+    await asyncio.sleep(minutes * 60)
 
     bot_status = "awake"
 
-
+@bot.tree.command(
+    name="awake",
+    description="反応するようになります"
+)
+async def awake(interaction: discord.Interaction,):
+    global bot_status
+    bot_status = "awake"
+    await interaction.response.send_message(f"おはつぐ～")
+    
 
 # discordと接続した時に呼ばれる
 @bot.event
