ba5a6cdeb0877bc78f45b1d64db04f33c0685746
hamu132 <guba06120@gmail.com>
2026-08-10 10:41:48 +0900

    ”」”と”くん”

--- extract_phrase.py
+++ extract_phrase.py
@@ -2,7 +2,7 @@ from sudachipy import Dictionary
 
 tokenizer = Dictionary().create()
 
-EXCLUDED_PREFIXES = {"」"}
+EXCLUDED_PREFIXES = {"」","くん"}
 def extract_phrase(text: str):
     tokens = list(tokenizer.tokenize(text))
 
